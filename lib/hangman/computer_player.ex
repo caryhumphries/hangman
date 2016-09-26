bc7c95f226370f5d5b3c1da88ee32a4b08323d4c
caryhumphries <chumphries@smu.edu>
2016-09-25 22:34:01 -0500

    Computer works in dumb mode

--- lib/hangman/computer_player.ex
+++ lib/hangman/computer_player.ex
@@ -4,43 +4,52 @@ defmodule Hangman.ComputerPlayer do
   alias Hangman.Game
 
   def play do
-    game   = Game.new_game
-    length = Game.word_length(game)
+    game = Game.new_game
+    word_length = Game.word_length(game)
     solver = %{
-      candidate_words: load_words_of_length(length),
+      candidate_words: load_words_of_length(word_length),
       letters_left:    (?a..?z) |>Enum.into([]),
     }
+
+    IO.inspect(solver.candidate_words)
 
     make_a_move(game, solver)
   end
 
   def make_a_move(game, solver) do
+    [ guess | t ] = solver.letters_left
+    #{ solver, guess } = get_guess(game, solver)
+    { game, status, guessed } = Game.make_move(game, <<guess::utf8>>)
+    analyze_move(status, game, %{solver | letters_left: t}, guess)
+    #analyze_move(status, game, solver, guess)
 
-    { solver, guess } = get_guess(game, solver)
-
-    { game, status, guess } = Game.make_move(game, guess)
-
-    analyze_move(status, game, solver, guess)
+    # IO.puts "Guess is: #{<<guess::utf8>>}"
+    # IO.inspect t
+    # IO.inspect status
+    # IO.inspect guessed
   end
 
   def analyze_move(:good_guess, game, solver, guess) do
-    IO.puts "#{inspect guess} was good"
+    IO.puts "#{inspect <<guess::utf8>>} was good"
     IO.puts Game.word_as_string(game)
     IO.puts ""
-    solver = remove_impossible(solver, game.word, guess)
+    #solver = remove_impossible(solver, game.word, guess)
+    #solver = remove_impossible(solver, Game.word_as_string(game), guess)
     make_a_move(game, solver)
   end
 
   def analyze_move(:bad_guess, game, solver, guess) do
-    IO.puts "#{inspect guess} was bad"
+    IO.puts "#{inspect <<guess::utf8>>} was bad"
     IO.puts Game.word_as_string(game)
     IO.puts ""
 
-    current = solver.candidate_words
-    new_words = remove_words_with_letter(current, guess)
-    |> remove_words_not_matching_pattern(game.word)
+    # current = solver.candidate_words
+    # new_words = remove_words_with_letter(current, guess)
+    # |> remove_words_not_matching_pattern(Game.word_as_string(game))
+    #|> remove_words_not_matching_pattern(game.word)
 
-    solver = %{ solver | candidate_words: new_words }
+    # solver = %{ solver | candidate_words: new_words }
+
     make_a_move(game, solver)
   end
 
@@ -53,35 +62,34 @@ defmodule Hangman.ComputerPlayer do
   end
 
 
-  def get_guess(game, solver) do
-    freqs = get_frequencies_of_letters(solver.candidate_words,
-                                       solver.letters_left)
-    |> Enum.sort(fn {_, a}, {_, b} -> a < b end)
-
-    {freqs, _} =
-      Enum.reduce(freqs,
-                  {[], 0},
-                  fn {ch, freq}, {result, cf} ->
-                    cf = cf + freq
-                    { [ { ch, cf} | result ], cf }
-                  end)
-
-    { ch, _ } = hd(freqs)
-    letters_left = solver.letters_left |> List.delete(ch)
-
-    solver = %{ solver | letters_left: letters_left }
-
-    ch = << ch :: utf8 >>
-
-    if guess_is_possible(solver, game[:word], ch) do
-      IO.inspect "#{ch} is possible"
-      { solver, ch }
-    else
-      IO.inspect "#{ch} is NOT possible"
-      get_guess(game, solver)
-    end
-  end
-
+  # def get_guess(game, solver) do
+  #   freqs = get_frequencies_of_letters(solver.candidate_words,
+  #                                      solver.letters_left)
+  #   |> Enum.sort(fn {_, a}, {_, b} -> a < b end)
+  #
+  #   {freqs, _} =
+  #     Enum.reduce(freqs,
+  #                 {[], 0},
+  #                 fn {ch, freq}, {result, cf} ->
+  #                   cf = cf + freq
+  #                   { [ { ch, cf} | result ], cf }
+  #                 end)
+  #
+  #   { ch, _ } = hd(freqs)
+  #   letters_left = solver.letters_left |> List.delete(ch)
+  #
+  #   solver = %{ solver | letters_left: letters_left }
+  #
+  #   ch = << ch :: utf8 >>
+  #
+  #   if guess_is_possible(solver, game[:word], ch) do
+  #     IO.inspect "#{ch} is possible"
+  #     { solver, ch }
+  #   else
+  #     IO.inspect "#{ch} is NOT possible"
+  #     get_guess(game, solver)
+  #   end
+  # end
 
 
   defp load_words_of_length(len) do
@@ -124,109 +132,109 @@ defmodule Hangman.ComputerPlayer do
     |> Enum.join
     |> Regex.compile!
   end
-
-
-  
-
-  # We need to know whether there is any place that a guess could
-  # fit. A new guess can only go where there's an underscore
-  # in the game state, so we check all the candidate words to
-  # see if our guess letter appears in any of them at any
-  # open spot.
-
-  defp guess_is_possible(solver, word, guess_ch) do
-    to_check = Enum.map(word, fn {_ch, known } -> !known end)
-    check_possibilities(solver.candidate_words, guess_ch, to_check)
-  end
-
-  defp check_possibilities([], _, _), do: false
-  defp check_possibilities([{candidate,_} | rest], guess_ch, to_check) do
-    possible =
-      candidate
-    |> String.codepoints
-    |> Enum.zip(to_check)
-    |> Enum.filter(fn {_, check} -> check end)
-    |> Enum.any?(fn {ch, _} -> ch == guess_ch end)
-
-    cond do
-      possible -> true
-      true     -> check_possibilities(rest, guess_ch, to_check)
-    end
-  end
-
-
-
-
-  defp remove_impossible(solver, word, guess_ch) do
-    to_check = Enum.map(word, fn {ch, known } -> ch == guess_ch && known end)
-    remove_words_that_conflict_with_current_pattern(solver, guess_ch, to_check)
-  end
-
-  defp remove_words_that_conflict_with_current_pattern(solver, guess_ch, to_check) do
-    candidates =
-      solver.candidate_words
-      |> Enum.filter(&(word_is_possible(&1, guess_ch, to_check)))
-    %{ solver | candidate_words: candidates }
-  end
-
-  defp word_is_possible({word, _}, guess_ch, to_check) do
-    word
-    |> String.codepoints
-    |> Enum.zip(to_check)
-    |> Enum.filter(fn {_, check} -> check end)
-    |> Enum.all?(fn {ch, _} -> ch == guess_ch end)
-  end
-
-  # defp check_candidates_for_letter_in_slot([], _, _) do
-  #   false
+  #
+  #
+  #
+  #
+  # # We need to know whether there is any place that a guess could
+  # # fit. A new guess can only go where there's an underscore
+  # # in the game state, so we check all the candidate words to
+  # # see if our guess letter appears in any of them at any
+  # # open spot.
+  #
+  # defp guess_is_possible(solver, word, guess_ch) do
+  #   to_check = Enum.map(word, fn {_ch, known } -> !known end)
+  #   check_possibilities(solver.candidate_words, guess_ch, to_check)
   # end
-  # 
-  # defp check_candidates_for_letter_in_slot([ {candidate,_} | rest], guess, to_check) do
-  #   chars = candidate |> String.codepoints
-  #   if check_one_word_for_letter_in_slot(chars, to_check, guess) do
-  #     true
-  #   else
-  #     check_candidates_for_letter_in_slot(rest, guess, to_check)
+  #
+  # defp check_possibilities([], _, _), do: false
+  # defp check_possibilities([{candidate,_} | rest], guess_ch, to_check) do
+  #   possible =
+  #     candidate
+  #   |> String.codepoints
+  #   |> Enum.zip(to_check)
+  #   |> Enum.filter(fn {_, check} -> check end)
+  #   |> Enum.any?(fn {ch, _} -> ch == guess_ch end)
+  #
+  #   cond do
+  #     possible -> true
+  #     true     -> check_possibilities(rest, guess_ch, to_check)
   #   end
   # end
-  # 
-  # defp check_one_word_for_letter_in_slot([], [], _) do
-  #   false
+  #
+  #
+  #
+  #
+  # defp remove_impossible(solver, word, guess_ch) do
+  #   to_check = Enum.map(word, fn {ch, known } -> ch == guess_ch && known end)
+  #   remove_words_that_conflict_with_current_pattern(solver, guess_ch, to_check)
   # end
-  # 
-  # defp check_one_word_for_letter_in_slot([actual | _rest_c],
-  #                                       [true   | _rest_t],
-  #                                       actual) do
-  #   true
+  #
+  # defp remove_words_that_conflict_with_current_pattern(solver, guess_ch, to_check) do
+  #   candidates =
+  #     solver.candidate_words
+  #     |> Enum.filter(&(word_is_possible(&1, guess_ch, to_check)))
+  #   %{ solver | candidate_words: candidates }
   # end
-  # 
-  # 
-  # 
-  # 
-  # defp check_one_word_for_letter_in_slot([_    | rest_c],
-  #                                       [_    | rest_t],
-  #                                       actual) do
-  #   check_one_word_for_letter_in_slot(rest_c, rest_t, actual)
+  #
+  # defp word_is_possible({word, _}, guess_ch, to_check) do
+  #   word
+  #   |> String.codepoints
+  #   |> Enum.zip(to_check)
+  #   |> Enum.filter(fn {_, check} -> check end)
+  #   |> Enum.all?(fn {ch, _} -> ch == guess_ch end)
   # end
-
-
-
-
-
-  defp get_frequencies_of_letters(candidates, letters_to_count) do
-    letters = Enum.map(letters_to_count, &{ &1, 1 <<< (&1-?a)})
-    Enum.reduce(candidates, %{}, &(count_for_one_word(&1, letters, &2)))
-  end
-
-  defp count_for_one_word({_word, signature}, letter_signatures, result) do
-    Enum.reduce(letter_signatures, result, fn {letter, letter_signature}, result ->
-      if (signature &&& letter_signature) != 0 do
-        Map.update(result, letter, 1, &(&1 + 1))
-      else
-        result
-      end
-    end)
-  end
+  #
+  # # defp check_candidates_for_letter_in_slot([], _, _) do
+  # #   false
+  # # end
+  # #
+  # # defp check_candidates_for_letter_in_slot([ {candidate,_} | rest], guess, to_check) do
+  # #   chars = candidate |> String.codepoints
+  # #   if check_one_word_for_letter_in_slot(chars, to_check, guess) do
+  # #     true
+  # #   else
+  # #     check_candidates_for_letter_in_slot(rest, guess, to_check)
+  # #   end
+  # # end
+  # #
+  # # defp check_one_word_for_letter_in_slot([], [], _) do
+  # #   false
+  # # end
+  # #
+  # # defp check_one_word_for_letter_in_slot([actual | _rest_c],
+  # #                                       [true   | _rest_t],
+  # #                                       actual) do
+  # #   true
+  # # end
+  # #
+  # #
+  # #
+  # #
+  # # defp check_one_word_for_letter_in_slot([_    | rest_c],
+  # #                                       [_    | rest_t],
+  # #                                       actual) do
+  # #   check_one_word_for_letter_in_slot(rest_c, rest_t, actual)
+  # # end
+  #
+  #
+  #
+  #
+  #
+  # defp get_frequencies_of_letters(candidates, letters_to_count) do
+  #   letters = Enum.map(letters_to_count, &{ &1, 1 <<< (&1-?a)})
+  #   Enum.reduce(candidates, %{}, &(count_for_one_word(&1, letters, &2)))
+  # end
+  #
+  # defp count_for_one_word({_word, signature}, letter_signatures, result) do
+  #   Enum.reduce(letter_signatures, result, fn {letter, letter_signature}, result ->
+  #     if (signature &&& letter_signature) != 0 do
+  #       Map.update(result, letter, 1, &(&1 + 1))
+  #     else
+  #       result
+  #     end
+  #   end)
+  # end
 
 
 
